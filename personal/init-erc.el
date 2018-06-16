6ba75413707895f5976704d4453f0e286cc03970
widp <tejaswidprakash@gmail.com>
2018-06-16 21:59:58 +0530

    WIP erc configuration

--- personal/init-erc.el
+++ personal/init-erc.el
@@ -1,10 +1,13 @@
 (require 'prelude-erc)
+(require 'erc)
 (setq my-fav-irc '("irc.freenode.net" 
                    "irc.oftc.net"
                    "irc.mozilla.org" 
                    "irc.gnome.org"
                    "irc.rizon.net"
                    "irc.atw-inter.net"))
+(setq erc-nick "widp")
+(setq bye-irc-message "...")
 (setq erc-autojoin-channels-alist 
       '(("irc.freenode.net" "#emacs" "#python" "#programming") 
         ("irc.rizon.net" "#/g/dpt" "#/g/sicp" "#etc")
