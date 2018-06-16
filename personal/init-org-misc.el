721001f0ed3de519f371b9a427ca3b67da08e93c
widp <tejaswidprakash@gmail.com>
2018-06-16 21:58:45 +0530

    org time stamp format changed

--- personal/init-org-misc.el
+++ personal/init-org-misc.el
@@ -4,3 +4,5 @@
 (require 'org-macs)
 (setq load-path (cons "/home/tejaswidp/.emacs.d/elpa/org-mind-map-20171206.645/" load-path))
 (require 'org-mind-map)
+(setq-default org-display-custom-times t)
+(setq org-time-stamp-custom-formats '("<%b %e %Y>" . "<%b %e %Y %H:%M>"))
