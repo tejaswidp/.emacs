f44d0f1de933cf035282b6ebb3e9945c5d162204
widp <tejaswidprakash@gmail.com>
2018-06-26 00:52:09 +0530

    better pdf-emacs integration, vim bindings & interleave-mode

--- personal/init-pdftools.el
+++ personal/init-pdftools.el
@@ -9,8 +9,19 @@
          (local-set-key (kbd "TAB") 'pdf-outline)
          (local-set-key (kbd "/") 'isearch-forward)
          (local-set-key (kbd "a") 'pdf-view-fit-page-to-window)
-         (local-set-key (kbd "s") 'pdf-view-fit-width-to-window)))
+         (local-set-key (kbd "s") 'pdf-view-fit-width-to-window)
+         (local-set-key (kbd ":") 'pdf-tools-ex)
+         (local-set-key (kbd "M-x") 'smex)
+         (local-set-key (kbd "C-o") 'interleave-open-notes-file-for-pdf)))
 
+(defun pdf-tools-ex()
+  (interactive)
+  (progn (setq input-page (read-from-minibuffer ":" ))
+         (pdf-view-goto-page (string-to-int input-page))))
+
+;;TODO: use the above keys here, make it an evil-pdftools minor mode.
+(defun pdf-tools-quit-ex()
+  (keyboard-quit))
 ;;(define-minor-mode pdf-evil-minor-mode
 ;;  "Evil minor mode for pdf tools"
 ;;    (progn (define-key (kbd "j") 'pdf-view-next-line-or-next-page)
@@ -21,3 +32,4 @@
 ;;         (define-key (kbd "/") 'isearch-forward)))
 (add-hook 'pdf-view-mode-hook 'evil-bindings-pdf-tools)
 (add-hook 'pdf-view-mode-hook 'pdf-tools-enable-minor-modes)
+;;(add-hook 'pdf-view-mode-hook 'interleave-open-notes-file-for-pdf )
