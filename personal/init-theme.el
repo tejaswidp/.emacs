c72874d53f6141e8f2e750e35d39618664ced32b
widp <tejaswidprakash@gmail.com>
2018-06-26 00:50:05 +0530

    change theme to twilight-bright

--- personal/init-theme.el
+++ personal/init-theme.el
@@ -1,2 +1,11 @@
 ;; hide minor modes
 (setq rm-blacklist ".*")
+
+;;(defun load-themer (x)
+;;  (progn
+;;        (mapc #'disable-theme custom-enabled-themes)
+;;        (load-theme x t)
+;;        (when (fboundp 'powerline-reset)
+;;          (powerline-reset))))
+;;(load-theme 'twilight-bright)
+;;(enable-theme 'twilight-bright)
